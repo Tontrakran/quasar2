a84a9aed26c9963ade474612a3c9df769a022836
Tontrakran <powerstong2@gmail.com>
2023-12-08 23:28:39 +0700

    Your name to Thai lang

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -9,8 +9,8 @@
       <q-input
         filled
         v-model="name"
-        label="Your name *"
-        hint="Name and surname"
+        label="ชื่อของคุณ *"
+        hint="ชื่อและนามสกุล"
         lazy-rules
         :rules="[ val => val && val.length > 0 || 'Please type something']"
       />
